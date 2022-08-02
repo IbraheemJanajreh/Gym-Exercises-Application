e64f42e10286ff28c876524bb19b3b094bfc0ea4
Ibraheem <rf.nadal.22@gmail.com>
2022-08-02 09:54:45 +0300

    added exercises types section

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import { fetchData, exerciseOptions } from "../utils/fetchData";
-const SearchExercises = () => {
+import HorizontalScrollbar from "./HorizontalScrollbar";
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState("");
-  const [exercises, setExercises] = useState([]);
   const [bodyParts, setBodyParts] = useState([]);
 
   useEffect(() => {
@@ -29,6 +29,7 @@ const SearchExercises = () => {
           exercise.equipment.toLowerCase().includes(search) ||
           exercise.bodyPart.toLowerCase().includes(search)
       );
+      console.log(searchedExercises);
       setSearch("");
       setExercises(searchedExercises);
     }
@@ -77,7 +78,11 @@ const SearchExercises = () => {
         </Button>
       </Box>
       <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
-        <HorizontalScrollbar data={bodyParts} />
+        <HorizontalScrollbar
+          data={bodyParts}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}
+        />
       </Box>
     </Stack>
   );
